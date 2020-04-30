38bf4aa84d542c3c96bc5ecaac929fb29e7b3efc
N-Zubko <63440229+N-Zubko@users.noreply.github.com>
2020-04-30 00:06:41 -0600

    Submit hometask8

---
+++ script.js
@@ -0,0 +1,14 @@
+var X;
+
+var Y;
+
+var x = prompt("Type any number");
+
+var y = prompt("Type another number");
+
+var X = parseInt(x,10);
+
+var Y = parseInt(y,10);
+
+var SUM = (X + Y);
+alert("The sum is " + SUM);
